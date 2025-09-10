b92bc87f736b6f08fc78b9d5be8a82566400f712
HypoxiE <kosmaer42@gmail.com>
2025-09-10 12:02:54 +0300

    Добавлен класс для принятия заявок на изменение данных

--- tournaments-get-form-api/http_admin_funcs/adminpost/update_result.go
+++ tournaments-get-form-api/http_admin_funcs/adminpost/update_result.go
@@ -1,13 +1,40 @@
 package adminpost
 
 import (
+	"encoding/json"
+	"io"
+	"net/http"
 	"tournaments-api/database"
 
 	"github.com/gin-gonic/gin"
 )
 
-// data_type: "metric", "metadata", "result"
-// {"result_id": 4, "tournament_id": 3, "data_type": "result", "update_data": [{"key": "version"}]}
-func UpdateResult(c *gin.Context, manager *database.DataBase) {
+//.Model(&user).Updates(User{Name: "Alice", Age: 30})
 
+type InputData struct {
+	DataType string          `json:"data_type"`
+	Data     json.RawMessage `json:"data"`
+}
+
+// data_type: "metric", "metadata", "result", "tournament"
+// {"data_type": "metric", "data": {"result_id": 1, "qwerty"}}
+func UpdateResult(c *gin.Context, manager *database.DataBase) {
+	body, err := io.ReadAll(c.Request.Body)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	var data InputData
+	if err := json.Unmarshal(body, &data); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	switch data.DataType {
+	case "metric":
+	case "metadata":
+	case "result":
+	case "tournament":
+	}
 }
