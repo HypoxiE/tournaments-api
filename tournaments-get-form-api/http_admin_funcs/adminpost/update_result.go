bdaeca7bd9904a1f387113eff9818a00a6629578
HypoxiE <kosmaer42@gmail.com>
2025-09-11 22:29:04 +0300

    Добавлена возможность принимать запрос на изменение массива данных

--- tournaments-get-form-api/http_admin_funcs/adminpost/update_result.go
+++ tournaments-get-form-api/http_admin_funcs/adminpost/update_result.go
@@ -13,12 +13,14 @@ import (
 //.Model(&user).Updates(User{Name: "Alice", Age: 30})
 
 type InputData struct {
-	DataType string            `json:"data_type"`
-	Data     []json.RawMessage `json:"data"`
+	Results     []json.RawMessage `json:"results"`
+	Metrics     []json.RawMessage `json:"metrics"`
+	Metadata    []json.RawMessage `json:"metadata"`
+	Tournaments []json.RawMessage `json:"tournaments"`
 }
 
 // data_type: "metric", "metadata", "result", "tournament"
-// {"data_type": "metric", "data": [{"result_id": 1, "qwerty"}]}
+// {"metrics": [], "metadata": [], "results": [], "tournaments": []}
 func UpdateResult(c *gin.Context, manager *database.DataBase) {
 	body, err := io.ReadAll(c.Request.Body)
 	if err != nil {
@@ -32,95 +34,96 @@ func UpdateResult(c *gin.Context, manager *database.DataBase) {
 		return
 	}
 
-	switch data.DataType {
-	case "metric":
-		set := make(map[uint]struct{})
-		for _, jsonMetric := range data.Data {
-			var metric classes.Metric
-			if err := json.Unmarshal(jsonMetric, &metric); err != nil {
-				c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-				return
-			}
-			db := manager.DataBase.Model(&metric).Updates(metric)
-			if db.Error != nil {
-				c.JSON(http.StatusBadRequest, gin.H{"error": db.Error})
-				return
-			}
-			if err := db.First(&metric, metric.ID).Error; err != nil {
-				c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-				return
-			}
-			set[metric.ResultID] = struct{}{}
+	// Metrics
+	set := make(map[uint]struct{})
+	for _, jsonMetric := range data.Metrics {
+		var metric classes.Metric
+		if err := json.Unmarshal(jsonMetric, &metric); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
 		}
-		unique := make([]string, 0, len(set))
-		for value := range set {
-			var result classes.Result
-			if err := manager.DataBase.Preload("Tournament").Preload("Metrics").First(&result, value).Error; err != nil {
-				c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-				return
-			}
-			result.CalculateScore(result.Tournament)
-
-			db := manager.DataBase.Model(&result).Updates(result)
-			if db.Error != nil {
-				c.JSON(http.StatusBadRequest, gin.H{"error": db.Error})
-				return
-			}
-			unique = append(unique, result.Username)
+		db := manager.DataBase.Model(&metric).Updates(metric)
+		if db.Error != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": db.Error})
+			return
 		}
-		c.JSON(http.StatusOK, gin.H{"updated": unique})
-	case "metadata":
-		for _, jsonMetadata := range data.Data {
-			var metadata classes.Metadata
-			if err := json.Unmarshal(jsonMetadata, &metadata); err != nil {
-				c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-				return
-			}
-			db := manager.DataBase.Model(&metadata).Updates(metadata)
-			if db.Error != nil {
-				c.JSON(http.StatusBadRequest, gin.H{"error": db.Error})
-				return
-			}
+		if err := db.First(&metric, metric.ID).Error; err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
 		}
-	case "result":
-		for _, jsonResult := range data.Data {
-			var result classes.Result
-			if err := json.Unmarshal(jsonResult, &result); err != nil {
-				c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-				return
-			}
-			db := manager.DataBase.Model(&result).Updates(result)
-			if db.Error != nil {
-				c.JSON(http.StatusBadRequest, gin.H{"error": db.Error})
-				return
-			}
-
-			if err := manager.DataBase.Preload("Tournament").Preload("Metrics").First(&result, result.ID).Error; err != nil {
-				c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-				return
-			}
-			result.CalculateScore(result.Tournament)
-
-			db = manager.DataBase.Model(&result).Updates(result)
-			if db.Error != nil {
-				c.JSON(http.StatusBadRequest, gin.H{"error": db.Error})
-				return
-			}
-
-			c.JSON(http.StatusOK, gin.H{"updated": result.Username})
+		set[metric.ResultID] = struct{}{}
+	}
+	unique := make([]string, 0, len(set))
+	for value := range set {
+		var result classes.Result
+		if err := manager.DataBase.Preload("Tournament").Preload("Metrics").First(&result, value).Error; err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
 		}
-	case "tournament":
-		for _, jsonTournament := range data.Data {
-			var tournament classes.Tournament
-			if err := json.Unmarshal(jsonTournament, &tournament); err != nil {
-				c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-				return
-			}
-			db := manager.DataBase.Model(&tournament).Updates(tournament)
-			if db.Error != nil {
-				c.JSON(http.StatusBadRequest, gin.H{"error": db.Error})
-				return
-			}
+		result.CalculateScore(result.Tournament)
+
+		db := manager.DataBase.Model(&result).Updates(result)
+		if db.Error != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": db.Error})
+			return
+		}
+		unique = append(unique, result.Username)
+	}
+	c.JSON(http.StatusOK, gin.H{"updated": unique})
+
+	// Metadata
+	for _, jsonMetadata := range data.Metadata {
+		var metadata classes.Metadata
+		if err := json.Unmarshal(jsonMetadata, &metadata); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+		db := manager.DataBase.Model(&metadata).Updates(metadata)
+		if db.Error != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": db.Error})
+			return
+		}
+	}
+
+	// Results
+	for _, jsonResult := range data.Results {
+		var result classes.Result
+		if err := json.Unmarshal(jsonResult, &result); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+		db := manager.DataBase.Model(&result).Updates(result)
+		if db.Error != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": db.Error})
+			return
+		}
+
+		if err := manager.DataBase.Preload("Tournament").Preload("Metrics").First(&result, result.ID).Error; err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+		result.CalculateScore(result.Tournament)
+
+		db = manager.DataBase.Model(&result).Updates(result)
+		if db.Error != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": db.Error})
+			return
+		}
+
+		c.JSON(http.StatusOK, gin.H{"updated": result.Username})
+	}
+
+	// Tournaments
+	for _, jsonTournament := range data.Tournaments {
+		var tournament classes.Tournament
+		if err := json.Unmarshal(jsonTournament, &tournament); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+		db := manager.DataBase.Model(&tournament).Updates(tournament)
+		if db.Error != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": db.Error})
+			return
 		}
 	}
 }
