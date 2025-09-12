e0a6b7206e849d6644654410e79ca0865e086997
HypoxiE <kosmaer42@gmail.com>
2025-09-13 01:21:11 +0300

    Очень сильно улучшена читаемость и оптимизирован код

--- tournaments-get-form-api/classes/funcs.go
+++ tournaments-get-form-api/classes/funcs.go
@@ -50,3 +50,17 @@ func TournamentDataFromJson(jsonData []byte, ip string) (Tournament, error) {
 
 	return tournament, nil
 }
+
+func GetUnique[T comparable](input []T) []T {
+	seen := make(map[T]struct{})
+	result := []T{}
+
+	for _, val := range input {
+		if _, ok := seen[val]; !ok {
+			seen[val] = struct{}{}
+			result = append(result, val)
+		}
+	}
+
+	return result
+}
--- tournaments-get-form-api/http_admin_funcs/adminpost/update_result.go
+++ tournaments-get-form-api/http_admin_funcs/adminpost/update_result.go
@@ -19,6 +19,147 @@ type InputData struct {
 	Tournaments []json.RawMessage `json:"tournaments"`
 }
 
+type ResponseData struct {
+	Results       []string
+	ResultsID     []uint
+	MetricsID     []uint
+	MetadataID    []uint
+	Tournaments   []string
+	TournamentsID []uint
+}
+
+func (response *ResponseData) unique() {
+	response.Results = classes.GetUnique(response.Results)
+	response.ResultsID = classes.GetUnique(response.ResultsID)
+	response.MetadataID = classes.GetUnique(response.MetadataID)
+	response.MetricsID = classes.GetUnique(response.MetricsID)
+	response.Tournaments = classes.GetUnique(response.Tournaments)
+	response.TournamentsID = classes.GetUnique(response.TournamentsID)
+}
+
+type UnpackMetricsOutput struct {
+	MetricsID   []uint
+	ResultsID   []uint
+	ResultsName []string
+}
+
+func UnpackMetrics(manager *database.DataBase, metrics []json.RawMessage) (UnpackMetricsOutput, error) {
+	var output UnpackMetricsOutput
+
+	set := make(map[uint]struct{})
+	for _, jsonMetric := range metrics {
+		var metric classes.Metric
+		if err := json.Unmarshal(jsonMetric, &metric); err != nil {
+			return output, err
+		}
+		db := manager.DataBase.Model(&metric).Updates(metric)
+		if db.Error != nil {
+			return output, db.Error
+		}
+		if err := db.First(&metric, metric.ID).Error; err != nil {
+			return output, db.Error
+		}
+		set[metric.ResultID] = struct{}{}
+		output.MetricsID = append(output.MetricsID, metric.ID)
+	}
+	for value := range set {
+		var result classes.Result
+		if err := manager.DataBase.Preload("Tournament").Preload("Metrics").First(&result, value).Error; err != nil {
+			return output, err
+		}
+		result.CalculateScore(result.Tournament)
+
+		db := manager.DataBase.Model(&result).Updates(result)
+		if db.Error != nil {
+			return output, db.Error
+		}
+
+		output.ResultsName = append(output.ResultsName, result.Username)
+		output.ResultsID = append(output.ResultsID, result.ID)
+	}
+	return output, nil
+}
+
+type UnpackMetadataOutput struct {
+	MetadataID []uint
+}
+
+func UnpackMetadata(manager *database.DataBase, metadata []json.RawMessage) (UnpackMetadataOutput, error) {
+	var output UnpackMetadataOutput
+	for _, jsonMetadata := range metadata {
+		var metadata classes.Metadata
+		if err := json.Unmarshal(jsonMetadata, &metadata); err != nil {
+			return output, err
+		}
+		db := manager.DataBase.Model(&metadata).Updates(metadata)
+		if db.Error != nil {
+			return output, db.Error
+		}
+		output.MetadataID = append(output.MetadataID, metadata.ID)
+	}
+	return output, nil
+}
+
+type UnpackResultsOutput struct {
+	ResultsID   []uint
+	ResultsName []string
+}
+
+func UnpackResults(manager *database.DataBase, results []json.RawMessage) (UnpackResultsOutput, error) {
+	var output UnpackResultsOutput
+
+	for _, jsonResult := range results {
+		var result classes.Result
+		if err := json.Unmarshal(jsonResult, &result); err != nil {
+			return output, err
+		}
+		db := manager.DataBase.Model(&result).Updates(result)
+		if db.Error != nil {
+			return output, db.Error
+		}
+
+		if err := manager.DataBase.Preload("Tournament").Preload("Metrics").First(&result, result.ID).Error; err != nil {
+			return output, err
+		}
+		result.CalculateScore(result.Tournament)
+		result.PublicToPrivate()
+
+		db = manager.DataBase.Model(&result).Updates(result)
+		if db.Error != nil {
+			return output, db.Error
+		}
+		output.ResultsName = append(output.ResultsName, result.Username)
+		output.ResultsID = append(output.ResultsID, result.ID)
+	}
+	return output, nil
+}
+
+type UnpackTournamentsOutput struct {
+	TournamentsID   []uint
+	TournamentsName []string
+	ResultsID       []uint
+	ResultsName     []string
+}
+
+func UnpackTournaments(manager *database.DataBase, tournaments []json.RawMessage) (UnpackTournamentsOutput, error) {
+	var output UnpackTournamentsOutput
+
+	for _, jsonTournament := range tournaments {
+		var tournament classes.Tournament
+		if err := json.Unmarshal(jsonTournament, &tournament); err != nil {
+			return output, err
+		}
+		db := manager.DataBase.Model(&tournament).Updates(tournament)
+		if db.Error != nil {
+			return output, db.Error
+		}
+		output.TournamentsID = append(output.TournamentsID, tournament.ID)
+		output.TournamentsName = append(output.TournamentsName, tournament.Name)
+	}
+
+	return output, nil
+}
+
 // data_type: "metric", "metadata", "result", "tournament"
 // {"metrics": [], "metadata": [], "results": [], "tournaments": []}
 func UpdateResult(c *gin.Context, manager *database.DataBase) {
@@ -34,104 +175,46 @@ func UpdateResult(c *gin.Context, manager *database.DataBase) {
 		return
 	}
 
-	changed_results_id := make(map[uint]struct{})
+	var response ResponseData
 
 	// Metrics
-	set := make(map[uint]struct{})
-	for _, jsonMetric := range data.Metrics {
-		var metric classes.Metric
-		if err := json.Unmarshal(jsonMetric, &metric); err != nil {
-			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-			return
-		}
-		db := manager.DataBase.Model(&metric).Updates(metric)
-		if db.Error != nil {
-			c.JSON(http.StatusBadRequest, gin.H{"error": db.Error})
-			return
-		}
-		if err := db.First(&metric, metric.ID).Error; err != nil {
-			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-			return
-		}
-		set[metric.ResultID] = struct{}{}
-		changed_results_id[metric.ResultID] = struct{}{}
-	}
-	for value := range set {
-		var result classes.Result
-		if err := manager.DataBase.Preload("Tournament").Preload("Metrics").First(&result, value).Error; err != nil {
-			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-			return
-		}
-		result.CalculateScore(result.Tournament)
-
-		db := manager.DataBase.Model(&result).Updates(result)
-		if db.Error != nil {
-			c.JSON(http.StatusBadRequest, gin.H{"error": db.Error})
-			return
-		}
+	if output, err := UnpackMetrics(manager, data.Metrics); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err})
+		return
+	} else {
+		response.MetricsID = append(response.MetricsID, output.MetricsID...)
+		response.ResultsID = append(response.ResultsID, output.ResultsID...)
+		response.Results = append(response.Results, output.ResultsName...)
 	}
 
 	// Metadata
-	for _, jsonMetadata := range data.Metadata {
-		var metadata classes.Metadata
-		if err := json.Unmarshal(jsonMetadata, &metadata); err != nil {
-			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-			return
-		}
-		db := manager.DataBase.Model(&metadata).Updates(metadata)
-		if db.Error != nil {
-			c.JSON(http.StatusBadRequest, gin.H{"error": db.Error})
-			return
-		}
-		changed_results_id[metadata.ResultID] = struct{}{}
+	if output, err := UnpackMetadata(manager, data.Metadata); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err})
+		return
+	} else {
+		response.MetadataID = append(response.MetadataID, output.MetadataID...)
 	}
 
 	// Results
-	for _, jsonResult := range data.Results {
-		var result classes.Result
-		if err := json.Unmarshal(jsonResult, &result); err != nil {
-			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-			return
-		}
-		db := manager.DataBase.Model(&result).Updates(result)
-		if db.Error != nil {
-			c.JSON(http.StatusBadRequest, gin.H{"error": db.Error})
-			return
-		}
-
-		if err := manager.DataBase.Preload("Tournament").Preload("Metrics").First(&result, result.ID).Error; err != nil {
-			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-			return
-		}
-		result.CalculateScore(result.Tournament)
-		result.PublicToPrivate()
-
-		db = manager.DataBase.Model(&result).Updates(result)
-		if db.Error != nil {
-			c.JSON(http.StatusBadRequest, gin.H{"error": db.Error})
-			return
-		}
-		changed_results_id[result.ID] = struct{}{}
+	if output, err := UnpackResults(manager, data.Results); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err})
+		return
+	} else {
+		response.ResultsID = append(response.ResultsID, output.ResultsID...)
+		response.Results = append(response.Results, output.ResultsName...)
 	}
-
-	unique := make([]uint, 0, len(set))
-	for value := range changed_results_id {
-		unique = append(unique, value)
-	}
-
-	c.JSON(http.StatusOK, gin.H{"updated": unique})
 
 	// Tournaments
-	for _, jsonTournament := range data.Tournaments {
-		var tournament classes.Tournament
-		if err := json.Unmarshal(jsonTournament, &tournament); err != nil {
-			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-			return
-		}
-		db := manager.DataBase.Model(&tournament).Updates(tournament)
-		if db.Error != nil {
-			c.JSON(http.StatusBadRequest, gin.H{"error": db.Error})
-			return
-		}
+	if output, err := UnpackTournaments(manager, data.Tournaments); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err})
+		return
+	} else {
+		response.Tournaments = append(response.Tournaments, output.TournamentsName...)
+		response.TournamentsID = append(response.TournamentsID, output.TournamentsID...)
+		response.Results = append(response.Results, output.ResultsName...)
+		response.ResultsID = append(response.ResultsID, output.ResultsID...)
 	}
+
+	response.unique()
+	c.JSON(http.StatusOK, gin.H{"updated": response})
 }
